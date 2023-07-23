aaa0e57c0a18ef67df8956ad610ef6bca216b897
Viacheslav Poturaev <nanopeni@gmail.com>
2023-07-23 12:36:22 +0200

    Bump Swagger UI version (#37)

--- v4cdn/cdn.go
+++ v4cdn/cdn.go
@@ -2,7 +2,7 @@ package v4cdn
 
 const (
 	// AssetsBase is a base URL for the assets.
-	AssetsBase = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.1/"
+	AssetsBase = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.19.1/"
 
 	// FaviconBase is a base URL for favicons.
 	FaviconBase = "https://petstore.swagger.io/"
