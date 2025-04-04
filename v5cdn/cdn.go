174e11ce687c42fe279acb7e44eefe0c99a9a735
Viacheslav Poturaev <nanopeni@gmail.com>
2025-04-04 11:06:38 +0200

    Update to v5.20.5 (#48)

--- v5cdn/cdn.go
+++ v5cdn/cdn.go
@@ -2,7 +2,7 @@ package v5cdn
 
 const (
 	// AssetsBase is a base URL for the assets.
-	AssetsBase = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/"
+	AssetsBase = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.20.5/"
 
 	// FaviconBase is a base URL for favicons.
 	FaviconBase = "https://petstore.swagger.io/"
