1ac7b65dc7e05b2dfbf6c02f4571d2874f564de8
Justen Stall <39888103+justenstall@users.noreply.github.com>
2025-05-03 13:29:17 -0400

    update version in v5cdn package and docs
    Signed-off-by: Justen Stall <39888103+justenstall@users.noreply.github.com>

--- v5cdn/cdn.go
+++ v5cdn/cdn.go
@@ -2,7 +2,7 @@ package v5cdn
 
 const (
 	// AssetsBase is a base URL for the assets.
-	AssetsBase = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.20.5/"
+	AssetsBase = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/"
 
 	// FaviconBase is a base URL for favicons.
 	FaviconBase = "https://petstore.swagger.io/"
